eb564926186beb76fa41b7dcaebf1b2aa114c135
Minh Duc Hoang <hoangminhduc0308@gmail.com>
2024-04-20 14:48:11 -0700

    added table functionality to checkout

--- shop.js
+++ shop.js
@@ -18,17 +18,33 @@ function checkout() {
   }
 
   var ce = document.getElementById("checkout");
-
+  var checkout_table = `
+    <tr>\n
+    <th>Product</th>
+    \n<th>Price</th>
+    \n<th>Quantity</th>
+    \n<th>Total</th>
+    \n</tr>`;
   var total = 0;
   for (let i = 0; i < product_list.length; i++) {
     let price_of_item = price_list[i] * input_list[i];
-    ce.innerHTML += `
-    <td>${product_list[i]}</td>
-    <td>${price_list[i]}</td>
-    <td>${input_list[i]}</td>
-    <td>${price_of_item}</td>
-    `;
+    // console.log(
+    //   `<tr>\n<td>${product_list[i]}</td>\n<td>${price_list[i]}</td>\n<td>${input_list[i]}</td>\n<td>${price_of_item}</td>\n</tr>`
+    // );
+    checkout_table += `
+    <tr>\n
+        <td>${product_list[i]}</td>\n
+        <td>$${price_list[i]}</td>\n
+        <td>${input_list[i]}</td>\n
+        <td>$${price_of_item}</td>\n
+    </tr>\n`;
     total += price_of_item;
   }
-  ce.textContent += `Total price: $${total}`;
+  checkout_table += `
+  <tr>\n
+    <td colspan=3>Total</td>\n
+    <td>$${total}</td>
+  </tr>`;
+  ce.innerHTML = checkout_table;
+  ce.style.display = "block";
 }
